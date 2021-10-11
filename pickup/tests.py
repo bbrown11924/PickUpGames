5779962227929c053fa9a28dfe872f542149fa53
Josh Galita <jgalita1@umbc.edu>
2021-10-11 10:52:05 -0400

    Add simple admin page test

--- pickup/tests.py
+++ pickup/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# Test cases to make sure that pages exist
+class PageExistenceTests(TestCase):
+
+    def test_admin_page_exists(self):
+        """
+        Makes sure the admin page can be accessed
+        """
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
