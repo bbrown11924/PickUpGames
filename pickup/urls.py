43e17eb5d166e957e66849391db57d23df00d905
Josh Galita <jgalita1@umbc.edu>
2021-10-21 19:38:16 -0400

    Add registration page view

--- pickup/urls.py
+++ pickup/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/', views.profile_list, name='Profile List')
+    path('profile/', views.profile_list, name='Profile List'),
+    path('register/', views.register, name='register'),
+    path('create_account/', views.create_account, name='create_account'),
 ]
--- pickup/views.py
+++ pickup/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
 from .models import Profile
 
@@ -8,6 +7,22 @@ from .models import Profile
 def index(request):
     return HttpResponse("Hello World. This is my test of creating a view")
 
+# view for page to register a new account
+def register(request):
+    return render(request, 'pickup/register.html', {})
+
+# view for creating an account
+# reached by an action from the register page
+def create_account(request):
+
+    # verify passwords match
+    if (request.POST["password"] != request.POST["confirm_password"]):
+        context = {"username": request.POST["username"],
+                   "email": request.POST["email"],
+                   "error": "Error: Passwords do not match.",}
+        return render(request, 'pickup/register.html', context)
+
+    return HttpResponse("Success!")
 
 def profile_list(request):
     profileList = Profile.objects.all()
