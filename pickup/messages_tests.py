7a3f7689e54b608951e422c20b12b2a469f18c6e
josephl4 <josephl4@umbc.edu>
2021-11-03 16:12:49 -0400

    set up test to make sure Messages model holds user message. Test ran without error.

--- pickup/messages_tests.py
+++ pickup/messages_tests.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
-from django.urls import reverse
-from django.db.utils import IntegrityError
-from models import Player
-from models import Messages
+from pickup.models import Player
+from pickup.models import Messages
+
 
 class messaging_tests(TestCase):
 
-    def test_send(self):
+    def test_message_exists(self):
         player1 = Player.objects.create_user("prof", "prof@umbc.edu", "cats")
         player1.save()
         player2 = Player.objects.create_user("student", "student@umbc.edu", "project")
         player2.save()
-
+        user_message = Messages.objects.create(sender= player1, reciever= player2, message= "Dont forget to commit when theres no errors!")
+        user_message.save()
+        self.assertEqual(Messages.objects.get(sender=player1), user_message)
--- pickup/models.py
+++ pickup/models.py
@@ -30,8 +30,8 @@ class Player(User):
         return relativedelta(datetime.date.today(), self.date_of_birth).years
 
 class Messages(models.Model):
-    sender = models.ForeignKey(User, related_name = "sender", on_delete=models.RESTRICT)
-    reciever = models.ForeignKey(User, related_name="reciever", on_delete=models.RESTRICT)
+    sender = models.ForeignKey(Player, related_name = "sender", on_delete=models.RESTRICT)
+    reciever = models.ForeignKey(Player, related_name="reciever", on_delete=models.RESTRICT)
     message = models.CharField(max_length= 1000)
 
 
