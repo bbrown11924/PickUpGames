ea7a051ddd614aa7e293b1b5da946b2e7c17facd
lmcallister20 <78047944+lmcallister20@users.noreply.github.com>
2021-11-04 16:18:51 -0400

    Update urls.py

--- pickup/urls.py
+++ pickup/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
-    path('profile/', views.profile_list, name='Profile List'),
     path('register/', views.register, name='register'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
