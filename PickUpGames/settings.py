62d54b33859c6d42b2ace96dc440afb370b06f78
bbrown11924 <bbrown4@umbc.edu>
2021-11-22 21:26:45 -0500

    Test staticfile storage

--- PickUpGames/settings.py
+++ PickUpGames/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
