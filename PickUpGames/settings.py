079bd050090a54ede219bc20d18ec3c75895e306
bbrown11924 <bbrown4@umbc.edu>
2021-11-22 21:32:31 -0500

    Remove requirement

--- PickUpGames/settings.py
+++ PickUpGames/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f5x6$l*scj2h2c6(@y59f7i5i8r17u^umrnzu@z+ig)f)irl#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'DEBUG' in os.environ:
+if 'HEROKU' in os.environ:
     DEBUG = False
 else:
     DEBUG = True
@@ -125,8 +125,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
