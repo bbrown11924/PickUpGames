dac0cda67953c690ec92b29cacef516a341646fc
Josh Galita <jgalita1@umbc.edu>
2021-12-06 22:36:51 -0500

    Clean up the UI and integrate features
    Registered the models with the admin page. Added some cancel
    buttons and modified others to use the history.back() javascript.
    Added links to the message system from the home page and profile
    pages for other players. Fixed a display issue with the messages
    page.

--- pickup/admin.py
+++ pickup/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Profile, Player, Parks, Schedule, FavoriteParks, \
+    EventSignup, Messages
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Player)
+admin.site.register(Parks)
+admin.site.register(Schedule)
+admin.site.register(FavoriteParks)
+admin.site.register(EventSignup)
+admin.site.register(Messages)
