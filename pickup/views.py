2b9c45b21ddee5ca355a25b97b64c6ccb9ce51a8
Josh Galita <jgalita1@umbc.edu>
2021-12-07 11:10:03 -0500

    Remove loop for removing self from messages list

--- pickup/views.py
+++ pickup/views.py
@@ -568,12 +568,8 @@ def new_message(request):
         search_text = input_form.cleaned_data["search_text"]
 
         # get the list of players
-        players = list(Player.objects.filter(username__contains=search_text))
-
-        for i in range(len(players)):
-            if players[i].username == user.username:
-                del players[i]
-                break
+        players = list(Player.objects.filter(Q(username__contains=search_text) &
+                                             ~Q(username=user.username)))
 
         context = {"players": players,
                    "search_input": search_text,
